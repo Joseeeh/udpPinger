f69f0d1443dfdd04568f06a0bb6b32bbc56f099b
mathiasrotheisen <32953983+mathiasrotheisen@users.noreply.github.com>
2020-09-14 12:40:51 +0200

    Create udpClient.js
    added assignment

---
+++ udpClient.js
@@ -0,0 +1,5 @@
+// var host = '';
+// var port = 8080;
+// var dgram = require('dgram');
+
+// For at sende en besked i UDP brug derved Buffer.from() og gem i variabel. Denne variabel kan dermed sendes med socket.send
