162b06cd6ec9d911b3144011c239bca0fb2b719b
Joseeeh <55131636+Joseeeh@users.noreply.github.com>
2020-09-24 16:42:30 +0200

    Delete udpServer.js

--- udpServer.js
+++
@@ -1,8 +0,0 @@
-//var dgram = require('dgram'); Brug dette libraries til UDP-connections
-//var server = dgram.createSocket('udp4');
-
-//server.on 'Hvad skal der ske når serveren begynder at lytte?'
-
-//server.on 'Hvad skal der ske når serveren modtager data? Brug her server.send() (Kig i dokumentationen)
-
-//server.bind(PORT, HOST); Brug denne til at bind jeres socket til en specifik port og IP-adresse.
